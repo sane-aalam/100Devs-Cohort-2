ba64d93df98b65bd69601ce599055051ff2899a6
Sane Aalam <saneraza78692@gmail.com>
2024-09-15 22:52:31 +0530

    week-01 + pending assignments

---
+++ week-01/Js Basic/Array Methods/array.js
@@ -0,0 +1,19 @@
+
+let fruits_container = ["Apple","Bananan","Penapple","Mango"];
+console.log(fruits_container)
+
+// pop into the last
+fruits_container.pop();
+console.log(fruits_container);
+
+// pop into the front 
+fruits_container.unshift();
+console.log(fruits_container);
+
+// push new element 
+fruits_container.push("newFruits");
+console.log(fruits_container)
+
+// push into the front
+fruits_container.shift();
+console.log(fruits_container)
